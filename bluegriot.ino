3b89d2672e945109e3737736c6cd8abed8933848
BaptisteFourcade <96148303+BaptisteFourcade@users.noreply.github.com>
2024-10-18 00:14:49 +0200

    Add files via upload

---
+++ bluegriot.ino
@@ -0,0 +1,50 @@
+const int lecture_Vpin = A0; // Lecture de la tension
+const int lecture_Ipin  = A1; // Lecture du courant 
+const int PWM_filtree = A2;
+const int PWMpin = 3;
+
+
+//Reglage sinus
+int index = 0;
+const int numSteps = 20; // Nombre d'etapes dans la sinusoïde
+const int sinValues[numSteps] ={0,3,11,23,38,54,70,84,
+94,99,99,94,84,70,54,38,23,11,3,0};
+int dataVpin;
+int dataIpin;
+int dataPWM_filtree;
+int time = 0;
+
+void setup() {
+  Serial.begin(9600) ; // Ouverture port série avec baudrate de 9600 (augmenter si besoin)
+  pinMode(lecture_Vpin, INPUT); // Entree des valeurs analogiques
+  pinMode(lecture_Ipin, INPUT);
+  
+  pinMode(3, OUTPUT);//Pin PWM
+
+  //TIMER 2
+  TCCR2A = _BV(COM2B1) | _BV(WGM21) | _BV(WGM20); 
+  TCCR2B = _BV(CS21) |_BV(WGM22);
+  OCR2A = 99;
+  OCR2B  = 20; 
+  TIMSK2 |= _BV(OCIE2A);
+  sei(); // Activation des interruptions globales  interupts(); 
+}
+
+void loop() {
+  dataVpin = analogRead(lecture_Vpin);
+  dataIpin = analogRead(lecture_Ipin);
+  dataPWM_filtree = analogRead(PWM_filtree);
+  Serial.println(String(dataVpin) + ';' + String(dataIpin) + ';' + String(dataPWM_filtree) + ';' + String(time));
+  delay(10);
+  time += 1;
+  Serial.flush();
+}
+
+//Interuption Timer 2
+ISR(TIMER2_COMPA_vect) {
+  OCR2B=sinValues[index];
+  if (index < numSteps - 1)
+    index++;
+  else
+    index=0;
+    } 
---
+++ bluegriot.py
@@ -0,0 +1,56 @@
+import serial
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Connexion à l'Arduino 
+Arduino = serial.Serial("COM3", baudrate=9600, timeout=1)
+
+# Stockage des données
+data = []
+compt = 0
+
+# Nombre de mesures faites
+while compt < 50:  # Par exemple 50 mesures
+    line = Arduino.readline().decode('utf-8').strip()  # Lecture décodage et enlever les espaces
+    if line:  # S'assurer qu'il y a bien des données
+        data.append(line)
+        compt += 1
+
+# Sauvegarder les données dans un fichier texte
+def write(L):
+    with open("data.txt", mode="w") as file:
+        for line in L:
+            file.write(line + '\n')
+
+write(data)
+
+# Charger les données depuis le fichier (3 colonnes : Vpin, Ipin, PWM_filtree, temps)
+Vpin, Ipin, PWM_filtree, temps = np.loadtxt("data.txt", delimiter=';', unpack=True)
+
+# Tracé des données
+plt.figure(figsize=(10, 6))
+
+# Tracer les courbes
+plt.figure()
+plt.plot(temps * 0.00001, Vpin * 5 / 1023, color='r')
+plt.title("Vpin (Tension)")
+plt.xlabel("Temps (s)")
+plt.ylabel("Tension (V)")
+plt.grid(True)
+
+
+plt.figure()
+plt.plot(temps * 0.00001, Ipin * 5 / 1023, color='g')
+plt.title("Ipin (Courant)")
+plt.xlabel("Temps (s)")
+plt.ylabel("Courant (A)")
+plt.grid(True)
+
+
+plt.figure()
+plt.plot(temps * 0.00001, PWM_filtree * 5 / 1023, color='b')
+plt.title("PWM_filtree (PWM)")
+plt.xlabel("Temps (s)")
+plt.ylabel("PWM")
+plt.grid(True)
+plt.show()
